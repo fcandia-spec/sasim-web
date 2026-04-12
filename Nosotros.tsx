2a31ebc567656c3d3b4cb0cddaaee1783e56d948
fcandia-spec <fcandia333@gmail.com>
2026-04-12 16:47:43 -0400

    Remove unused icon rendering in Nosotros.tsx

---
+++ Nosotros.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Nosotros = () => {
+    return (
+        <div>
+            <h1>Nosotros</h1>
+            {/* Other content */}
+        </div>
+    );
+};
+
+export default Nosotros;
