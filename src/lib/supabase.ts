2a16219adca2d92f3cc702be8256e84c4e186150
fcandia-spec <fcandia333@gmail.com>
2026-04-24 21:36:00 -0400

    Actualizar supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,4 +4,3 @@ const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 const SUPABASE_ANON = import.meta.env.VITE_SUPABASE_ANON;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON);
-export { SUPABASE_URL };
